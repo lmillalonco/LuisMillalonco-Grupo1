eee77255c0fdd64e54eaad3c0dae790353adb0fb
Chamitox <luisignacio.millalonco@alumnos.ulagos.cl>
2024-09-09 19:27:02 -0300

    mi proyecto react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header/Header";
+import Contenido from "./components/Contenido/Contenido";
+import Footer from "./components/Footer/Footer";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Contenido />
+      <Footer />
+    </div>
+  );
+}
